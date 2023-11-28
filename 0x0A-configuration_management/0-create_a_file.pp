ada118377bbc5188da383308ceaddfbe9a0e9151
Mmadineo <mmadineo.seleke@gmail.com>
2023-11-28 02:22:37 -0800

    Task 0

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,7 @@
 #Create a file in /tmp
-file { '/tmp/school':
-     ensure	=> file,
-     mode	=> '07744',
-     owner	=> 'www-data',
-     group	=> 'www-data',
-     content	=> 'I love Puppet',
+file {  '/tmp/school':
+   mode		=>  '07744',
+   owner	=>  'www-data',
+   group	=>  'www-data',
+   content	=>  'I love Puppet',
 }
