53cfa09c70ad6c97e660e4a37b433d0530f3785c
Mmadineo <mmadineo.seleke@gmail.com>
2023-11-28 02:27:07 -0800

    Task 1 correction

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,5 @@
-#!/usr/bin/pup
-# Install flask from pip3
-package {'flask':
-provider => 'pip3',
-ensure => '2.1.0',
+#Install flask from pip3
+package {  'flask':
+provider	=> 'pip3',
+ensure		=> '2.1.0',
 }
